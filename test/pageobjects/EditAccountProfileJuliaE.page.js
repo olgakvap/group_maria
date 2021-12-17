202863607829f048e0b6b5a996bdedc41c3e2f70
JuliaEde <iyulok@gmail.com>
2021-12-17 15:20:29 -0800

    I added POM page for EditAccountProfile and I added 1 element to POM ViewProfilePage (the element takes the unordered list from hamburger menu, I am not sure if I should use get or wrap it in an async, because in order to get this element, the hamburger button has to be clicked) About the new EditAccountProfile POM page: I added all elements from the Edit Account Page, the only problem that I have is this line - return super.open('/user/61a639262d347ec0559fe77d/edit'); I don't know how to get User Id, that's why I created the new element in the ViewProfile.page.js, because it has the link to User profile with the User ID

---
+++ test/pageobjects/EditAccountProfileJuliaE.page.js
@@ -0,0 +1,33 @@
+const Page = require('./Page');
+class EditAccountProfileJuliaEPage extends Page {
+    get inputFirstName() {
+        return $('#first-name');
+    }
+    get inputLastName() {
+        return $('#last-name');
+    }
+    get inputJobTitle() {
+        return $('#job-title');
+    }
+    get inputExternalLinkToProfileImage() {
+        return $('#image');
+    }
+    get inputAbout() {
+        return $('#about');
+    }
+    get inputProgrammingLanguages() {
+        return $('#languages');
+    }
+    get btnCancel() {
+        return $("//button[@class='MuiButton-root MuiButton-outlined MuiButton-outlinedPrimary MuiButton-sizeMedium MuiButton-outlinedSizeMedium MuiButtonBase-root mt-5 mr-3 css-79xub']");
+    }
+    get btnSave() {
+        return $("//span[@class='MuiButton-startIcon MuiButton-iconSizeMedium css-6xugel']");
+    }
+
+    open() {
+        return super.open('/user/61a639262d347ec0559fe77d/edit');
+    }
+}
+
+module.exports = new EditAccountProfileJuliaEPage();
