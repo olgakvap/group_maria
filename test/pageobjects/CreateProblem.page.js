b30cc8208783eafc0369c349c6c4df56d4b0808e
Ilia <ponomidya.ru>
2021-12-21 14:22:38 -0800

    Refactoring and add new test

--- test/pageobjects/CreateProblem.page.js
+++ test/pageobjects/CreateProblem.page.js
@@ -16,9 +16,20 @@ class CreateProblemPage extends Page {
     get inputContent(){
         return $("//textarea[@class='w-md-editor-text-input ']");
     }
-
     get btnSave() {
         return $('[type="submit"]');
+    }
+    get errorSameTitle() {
+        return $("//div[@class='MuiAlert-message css-1w0ym84']");
+    }
+
+    async fillAndSave (title, position, content) {
+        await this.inputTitle.setValue(title);
+        await this.inputCompany.click();
+        await this.appleCompanyOption.click();
+        await this.inputPosition.setValue(position);
+        await this.inputContent.setValue(content);
+        await this.btnSave.click();
     }
 
 }
---
+++ test/specs/problemCreate.spec.js
@@ -0,0 +1,30 @@
+const LoginPage = require('../pageobjects/Login.page');
+const PublicationsPage = require('../pageobjects/Publications.page');
+const ProblemsPage = require('../pageobjects/Problems.page');
+const CreateProblemPage = require('../pageobjects/CreateProblem.page');
+
+describe('Creating Problem', () => {
+
+    const inputValueTitle = Date.now();
+
+    it('Create new problem with existing company', async () => {
+        await LoginPage.login(process.env.USER_EMAIL, process.env.USER_PASSWORD);
+        await PublicationsPage.btnHumburgerMenu.click();
+        await PublicationsPage.btnProblems.click();
+        await ProblemsPage.btnAddProblem.click();
+        await CreateProblemPage.fillAndSave(inputValueTitle, "any text", "any text");
+
+        await expect(ProblemsPage.firstRecord.getText().hasOwnProperty(inputValueTitle));
+    });
+
+    it('Create new problem with existing company and existing title', async () => {
+        let inputSameValueTitle = inputValueTitle;
+
+        await ProblemsPage.btnAddProblem.click();
+        await CreateProblemPage.fillAndSave(inputSameValueTitle, "any text", "any text");
+
+        await expect(CreateProblemPage.errorSameTitle.isDisplayed());
+        await expect(CreateProblemPage.errorSameTitle.toHaveText("Error: Problem with same title already exist"));
+    });
+
+});
