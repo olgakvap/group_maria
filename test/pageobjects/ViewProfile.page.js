45c35c6aba85089cb35ef2931bbaaebd51a0e362
JuliaEde <89672280+JuliaEde@users.noreply.github.com>
2021-12-23 11:13:52 -0800

    Added some elements to the POM file, removed one element that is in t… (#26)
    * Added some elements to the POM file, removed one element that is in the NavBar now
    
    * Update ViewProfile.page.js
    
    Co-authored-by: JuliaEde <iyulok@gmail.com>

--- test/pageobjects/ViewProfile.page.js
+++ test/pageobjects/ViewProfile.page.js
@@ -13,12 +13,32 @@ class ViewProfilePage extends Page {
     get userEmail() {
         return $("//div[@xpath='1']");
     }
+    get userAbout() {
+        return $("//b[contains(text(),'About:')]");
+    }
+    get userLanguages() {
+        return $("//b[contains(text(),'Languages:')]");
+    }
+    get userLevel() {
+        return $("//b[contains(text(),'Level:')]");
+    }
+    get userStarIcon() {
+        return $("[data-testid='StarIcon']");
+    }
+    get userRole() {
+        return $("//span[@class='MuiChip-label MuiChip-labelMedium css-9iedg7']");
+    }
+    get userInitials() {
+        return $("//div[@class='profile-image initials']");
+    }
+    //Todo ask about langauges, if there are more than one language on the profile,
+    // then this selector appears according to the number //span[@class='MuiChip-label MuiChip-labelSmall css-1pjtbja']
+    // of languages (1 time for 1 lang, two times for three and so on. We can use this selector to count them, probably
     get btnEdit() {
         return $("//button[contains(text(),'Edit')]");
     }
-
-    get goToProfilePage() {
-        return $("//ul[@class='MuiList-root MuiList-padding css-1ontqvh']");
+    get btnBackLink() {
+        return $("//div[@class='btn btn-link']");
     }
 
     open(id) {
