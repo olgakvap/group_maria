03a261fa5f2019cf6965c77e2339c9b595e23290
JuliaEde <89672280+JuliaEde@users.noreply.github.com>
2021-12-28 19:50:18 -0800

    Added one more test for Input Fields Limit Negative Testing (#37)
    * Added one more test for Input Fields Limit Negative Testing
    
    * Update profileEditNegative.spec.js
    
    * Update profileEditNegative.spec.js
    
    Co-authored-by: JuliaEde <iyulok@gmail.com>

--- test/ui/specs/user/profileEditNegative.spec.js
+++ test/ui/specs/user/profileEditNegative.spec.js
@@ -6,57 +6,62 @@ const {clearInputValue} = require('../../../helpers/uiMethods.helper');
 
 describe('Checking Input Fields Limit Negative Tests', () => {
 
-    it('First Name Max Length Validation (35 characters)', async () => {
+    before('Login with Existing User', async () => {
         await LoginPage.login(process.env.USER_EMAIL, process.env.USER_PASSWORD);
+    })
+
+    beforeEach('Open user profile page', async() => {
         await PublicationsPage.navBar.openProfile();
         await ViewProfilePage.btnEdit.click();
+    });
+
+    it('First Name Max Length Validation (35 characters)', async () => {
         await clearInputValue(await EditProfilePage.inputFirstName);
         await EditProfilePage.inputFirstName.setValue('I wish you a Merry Christmas and a Happy New Year!');
         await EditProfilePage.btnSave.click();
         await expect(EditProfilePage.errorMessage).toHaveText('ValidationError: firstName: Max length is 35 characters');
-        await PublicationsPage.navBar.logout();
 
     });
 
     it('Last Name Max Length Validation (35 characters)', async () => {
-        await LoginPage.login(process.env.USER_EMAIL, process.env.USER_PASSWORD);
-        await PublicationsPage.navBar.openProfile();
-        await ViewProfilePage.btnEdit.click();
         await clearInputValue(await EditProfilePage.inputLastName);
         await EditProfilePage.inputLastName.setValue('I wish you a Merry Christmas and a Happy New Year!');
         await EditProfilePage.btnSave.click();
         await expect(EditProfilePage.errorMessage).toHaveText('ValidationError: lastName: Max length is 35 characters');
-        await PublicationsPage.navBar.logout();
 
     });
 
     it('Job Title Max Length Validation (255 characters)', async () => {
-        await LoginPage.login(process.env.USER_EMAIL, process.env.USER_PASSWORD);
-        await PublicationsPage.navBar.openProfile();
-        await ViewProfilePage.btnEdit.click();
         await clearInputValue(await EditProfilePage.inputJobTitle);
         await EditProfilePage.inputJobTitle.setValue("Oh the weather outside is frightful, But the fire is so delightful, And since we've no place to go, Let It Snow! Let It Snow! Let It Snow!It doesn't show signs of stopping, And I've bought some corn for popping, The lights are turned way down low, Let It!!!");
         await EditProfilePage.btnSave.click();
         await expect(EditProfilePage.errorMessage).toHaveText('ValidationError: jobTitle: Max length is 255 characters');
-        await PublicationsPage.navBar.logout();
 
     });
 
     it('First Name and Last Name Max Length Validation', async () => {
-        await LoginPage.login(process.env.USER_EMAIL, process.env.USER_PASSWORD);
-        await PublicationsPage.navBar.openProfile();
-        await ViewProfilePage.btnEdit.click();
         await clearInputValue(await EditProfilePage.inputFirstName);
         await EditProfilePage.inputFirstName.setValue('I wish you a Merry Christmas and a Happy New Year!');
         await clearInputValue(await EditProfilePage.inputLastName);
         await EditProfilePage.inputLastName.setValue('I wish you a Merry Christmas and a Happy New Year!');
         await EditProfilePage.btnSave.click();
         await expect(EditProfilePage.errorMessage).toHaveText('ValidationError: firstName: Max length is 35 characters, lastName: Max length is 35 characters');
-        await PublicationsPage.navBar.logout();
 
     });
 
-});
+    it('Should Show Error Message for All Fields', async () => {
+        await clearInputValue(await EditProfilePage.inputFirstName);
+        await EditProfilePage.inputFirstName.setValue('I wish you a Merry Christmas and a Happy New Year!');
+        await clearInputValue(await EditProfilePage.inputLastName);
+        await EditProfilePage.inputLastName.setValue('I wish you a Merry Christmas and a Happy New Year!');
+        await clearInputValue(await EditProfilePage.inputJobTitle);
+        await EditProfilePage.inputJobTitle.setValue("Oh the weather outside is frightful, But the fire is so delightful, And since we've no place to go, Let It Snow! Let It Snow! Let It Snow!It doesn't show signs of stopping, And I've bought some corn for popping, The lights are turned way down low, Let It!!!");
+        await clearInputValue(await EditProfilePage.inputExternalLinkToProfileImage);
+        await EditProfilePage.inputExternalLinkToProfileImage.setValue("Oh the weather outside is frightful, But the fire is so delightful, And since we've no place to go, Let It Snow! Let It Snow! Let It Snow!It doesn't show signs of stopping, And I've bought some corn for popping, The lights are turned way down low, Let It!!!");
+        await clearInputValue(await EditProfilePage.inputAbout);
+        await EditProfilePage.inputAbout.setValue('"Oh the weather outside is frightful, But the fire is so delightful, And since weve no place to go, Let It Snow! Let It Snow! Let It Snow!It doesnttt show signs of stopping, And Ive bought some corn for popping, The lights are turned way down low, Let It!!!"');
+        await EditProfilePage.btnSave.click();
+        await expect(EditProfilePage.errorMessage).toHaveText('ValidationError: firstName: Max length is 35 characters, lastName: Max length is 35 characters, about: Max length is 255 characters, image: Max length is 255 characters, jobTitle: Max length is 255 characters');
+    });
 
-//Todo: Add negative tests for FirstName & Job Title, LastName & JobTitle, FirstName & LastName & JobTitle
-// also add Before Each and After Each
+});
