6eb71204dbaa264a860928a0dd0610a5a859b721
JuliaEde <89672280+JuliaEde@users.noreply.github.com>
2021-12-28 19:51:16 -0800

    Sorted the elements and async methods for NavBar.js component, added … (#38)
    * Sorted the elements and async methods for NavBar.js component, added 2 el and 2 asyncs
    
    * Update NavBar.js
    
    Co-authored-by: JuliaEde <iyulok@gmail.com>

--- test/ui/pageobjects/components/NavBar.js
+++ test/ui/pageobjects/components/NavBar.js
@@ -1,27 +1,40 @@
 module.exports = class NavBar {
     get pageTitle() { return $('h6'); }
     get btnHamburgerMenu() { return $("//button[@id='nav-bar-toggle']"); }
+    get btnPublications() { return $("#publications"); }
+    get btnPeople() { return $("#people"); }
+    get btnCompanies() { return $("#companies"); }
     get btnProblems() { return $('#problems'); }
     get btnProfile() { return $("#profile"); }
-    get btnPeople() { return $("#people"); }
     get logoutBtn() { return $("//span[.='Logout']"); }
 
+    async openPublications() {
+        await (await this.btnHamburgerMenu).click();
+        await (await this.btnPublications).click();
+    }
+    
+    async openPeople() {
+        await (await this.btnHamburgerMenu).click();
+        await (await this.btnPeople).click();
+    }
+    
+    async openCompanies() {
+        await (await this.btnHamburgerMenu).click();
+        await (await this.btnCompanies).click();
+    }
+    
     async openProblems() {
         await (await this.btnHamburgerMenu).click();
         await (await this.btnProblems).click();
     }
-
+    
     async openProfile() {
         await (await this.btnHamburgerMenu).click();
         await (await this.btnProfile).click();
     }
+    
     async logout() {
         await (await this.btnHamburgerMenu).click();
         await (await this.logoutBtn).click();
-    }
-
-    async openPeople() {
-        await (await this.btnHamburgerMenu).click();
-        await (await this.btnPeople).click();
     }
 }
