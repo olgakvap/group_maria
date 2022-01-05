8ae006630412c2fd21e52afea0a4d2136496795e
JuliaEde <iyulok@gmail.com>
2022-01-04 22:41:54 -0800

    Added method to get a random String with a certain length, the length can be random if we use test/data/invalidInputLength.js

--- test/helpers/uiMethods.helper.js
+++ test/helpers/uiMethods.helper.js
@@ -10,6 +10,16 @@ async function getValidationMessage(element){
     return requiredMessage;
 }
 
+async function randomString(length) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~!@#$%^&*()_+=-';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
 module.exports = {
     clearInputValue,
     getValidationMessage
