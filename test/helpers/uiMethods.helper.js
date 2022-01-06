796dd36fb2bbfad255385f59afab044e775688c8
JuliaEde <89672280+JuliaEde@users.noreply.github.com>
2022-01-05 19:02:31 -0800

    small commit, added ui random string method (#54)
    Co-authored-by: JuliaEde <iyulok@gmail.com>

--- test/helpers/uiMethods.helper.js
+++ test/helpers/uiMethods.helper.js
@@ -9,8 +9,18 @@ async function getValidationMessage(element){
     const requiredMessage = await browser.execute(`return document.getElementById("${element}").validationMessage`);
     return requiredMessage;
 }
+async function randomString(length){
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~!@#$%^&*()_+=-';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
 
 module.exports = {
     clearInputValue,
+    randomString,
     getValidationMessage
 }
