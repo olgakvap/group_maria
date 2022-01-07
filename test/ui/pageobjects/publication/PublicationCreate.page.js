1dee451b8e1bd28dfbf2ffe63c34ea108fd06d6f
mayboroda1985 <60754560+mayboroda1985@users.noreply.github.com>
2022-01-07 16:17:59 -0500

    refactored publicationCreate.spec.js and PublicationCreate.page.js (#53)
    * refactored publicationCreate.spec.js and PublicationCreate.page.js
    
    * refactored publicationCreate.spec.js and PublicationCreate.page.js

--- test/ui/pageobjects/publication/PublicationCreate.page.js
+++ test/ui/pageobjects/publication/PublicationCreate.page.js
@@ -1,4 +1,5 @@
 const Page = require('../Page');
+const {clearInputValue} = require ('../../../helpers/uiMethods.helper');
 
 class PublicationCreatePage extends Page {
     get inputTitle() {
@@ -10,15 +11,42 @@ class PublicationCreatePage extends Page {
     get textareaContent() {
         return $('.w-md-editor-text-input');
     }
+     get inputImageLink() {
+      return $('//input[@id="image"]');
+    }
+
     get btnSavePublication() {
-        return $('[type="submit"]');
+        return $('//button[@type="submit"]');
     }
     get btnCancelPublication() {
-        return $('//button[contains(text(),"Cancel")]');
+        return $('//button[contains(@class,"mr-3 css-79xub")]');
     }
     get errorMessage() {
-        return $('//div[@class=\'MuiAlert-message css-1w0ym84\']');
+        return $('//div[@class="MuiAlert-message css-1w0ym84"]');
     }
+
+    async fillAndSave(title = '', description = '', content = '', image = null) {
+        await clearInputValue(this.inputTitle);
+        await this.inputTitle.setValue(title);
+        await clearInputValue(this.inputImageLink);
+        await this.inputImageLink.setValue(image);
+        await clearInputValue(this.inputDescription);
+        await this.inputDescription.setValue(description);
+        await clearInputValue(this.textareaContent);
+        await this.textareaContent.setValue(content);
+        await this.btnSavePublication.click();
+
+    }
+
+    async fillAndCancel(title = '', description = '', content = '', image = null) {
+        await this.inputTitle.setValue(title);
+        await this.inputImageLink.setValue(image);
+        await this.inputDescription.setValue(description);
+        await this.textareaContent.setValue(content);
+        await this.btnCancelPublication.click();
+        await browser.refresh();
+    }
+
     open() {
         return super.open('/publications/create');
     }
--- test/ui/specs/publication/publicationCreate.spec.js
+++ test/ui/specs/publication/publicationCreate.spec.js
@@ -1,66 +1,76 @@
 const LoginPage = require('../../pageobjects/auth/Login.page');
 const PublicationsPage = require('../../pageobjects/publication/Publications.page');
-const CreatePublicationPage = require('../../pageobjects/publication/PublicationCreate.page');
+const PublicationCreatePage = require('../../pageobjects/publication/PublicationCreate.page');
 const expected = require('../../../data/expected.json');
 const {getValidationMessage} = require("../../../helpers/uiMethods.helper");
+const {clearInputValue} = require ('../../../helpers/uiMethods.helper');
 
-// TODO: requires refactoring / reviewing
-describe('Creating Publication', () => {
+describe('Creating Publication - Positive', () => {
 
     before('Open login page', async () => {
         await LoginPage.login(process.env.USER_EMAIL, process.env.USER_PASSWORD);
     });
 
-    xit('Should create the Publication providing required information', async () => {
-        const publicationTitle = `publication${Date.now()}`;
+    beforeEach('Open Create Page', async () => {
         await PublicationsPage.btnAddPublication.click();
-        await CreatePublicationPage.inputTitle.setValue(publicationTitle);
-        await CreatePublicationPage.inputDescription.setValue("New Position2");
-        await CreatePublicationPage.textareaContent.setValue("Bachelor's degree in a related field or equivalent practical experience.");
-        await CreatePublicationPage.btnSavePublication.click();
+    });
+
+   it('Should create the Publication providing required information', async () => {
+        const publicationTitle = `publication${Date.now()}`;
+        await PublicationCreatePage.fillAndSave(publicationTitle, "QA position","This is a description field.");
         await expect(PublicationsPage.navBar.pageTitle).toHaveText('publications');
         //Todo: add verification that publication with specified title has been created
-        await PublicationsPage.linkLoadMore.waitForDisplayed();
-        const publication = await PublicationsPage.findPublication(publicationTitle);
-        expect(await PublicationsPage.getPublicationTitle(publication)).toBe(publicationTitle);
+        //await PublicationsPage.linkLoadMore.waitForDisplayed();
+        //const publication = await PublicationsPage.findPublication(publicationTitle);
+        //expect(await PublicationsPage.getPublicationTitle(publication)).toBe(publicationTitle);
+
     });
 
-    xit ('Should not create the Publication without Title', async () => {
-        await PublicationsPage.btnAddPublication.click();
-        await CreatePublicationPage.btnSavePublication.click();
+    it('Should create the Publication providing required information and image', async () => {
+        const publicationTitle = `publication${Date.now()}`;
+        const inputImageLink = `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTA6xOLhJmOjhQjqHsuCPSL1-_2RCbCve1keg&usqp=CAU${Date.now()}`;
+        await PublicationCreatePage.fillAndSave(publicationTitle, "QA position","This is a description field.", inputImageLink );
+        await expect(PublicationsPage.navBar.pageTitle).toHaveText('publications');
+        //Todo: add verification that publication with specified title has been created
+        //await PublicationsPage.linkLoadMore.waitForDisplayed();
+        //const publication = await PublicationsPage.findPublication(publicationTitle);
+        //expect(await PublicationsPage.getPublicationTitle(publication)).toBe(publicationTitle);
 
-        const requiredMessage = await getValidationMessage('title');
-        //await expect(CreatePublicationPage.errorMessage).toBeDisplayed();
-        await expect(requiredMessage).toHaveText(expected.general.errors.REQUIRED_FIELD);
     });
 
-    xit('Should not create the Publication without Description', async () => {
-        await PublicationsPage.btnAddPublication.click();
-        await CreatePublicationPage.inputTitle.setValue('Apple Company');
-        await CreatePublicationPage.btnSavePublication.click();
-        await expect(CreatePublicationPage.errorMessage).toBeDisplayed();
-        await expect(CreatePublicationPage.errorMessage).toHaveText(expected.general.errors.REQUIRED_FIELD);
-    });
-
-    // TODO: cant runt last scenario with this one
-    it('Should not create the Publication without Content', async () => {
-        await PublicationsPage.btnAddPublication.click();
-        await CreatePublicationPage.inputTitle.setValue('Apple Company');
-        await CreatePublicationPage.inputDescription.setValue('Information about apple company');
-        await CreatePublicationPage.btnSavePublication.click();
-        await expect(CreatePublicationPage.errorMessage).toBeDisplayed();
-        await expect(CreatePublicationPage.errorMessage).toHaveText(expected.publications.errors.CONTENT_REQUIRED_FIELD);
-        await CreatePublicationPage.btnCancelPublication.click();
-        await browser.refresh();
-    });
-
-    it('Should "CANCEL" creating Publication', async () => {
-        await PublicationsPage.btnAddPublication.click();
-        await CreatePublicationPage.inputTitle.setValue('Google QA Engineer Position');
-        await CreatePublicationPage.inputDescription.setValue('New Position');
-        await CreatePublicationPage.textareaContent.setValue('Minimum qualifications');
-        await CreatePublicationPage.btnCancelPublication.click();
+    xit('Should "CANCEL" creating Publication', async () => {
+        await PublicationCreatePage.fillAndCancel('Google QA Engineer Position', 'New Position','Minimum qualifications');
+        await expect(PublicationsPage.navBar.pageTitle).toHaveText('publications');
     });
 });
 
+describe.only('Creating Publication - Negative', () => {
 
+    before('Open login page', async () => {
+        await LoginPage.login(process.env.USER_EMAIL, process.env.USER_PASSWORD);
+        await PublicationsPage.btnAddPublication.click();
+    });
+
+    it('Should not create the Publication without Description', async () => {
+        await PublicationCreatePage.inputTitle.setValue('Apple Company');
+        await PublicationCreatePage.btnSavePublication.click();
+        const requiredMessage = await getValidationMessage('description');
+        await expect(requiredMessage).toEqual(expected.general.errors.REQUIRED_FIELD);
+    });
+
+    it.only('Should not create the Publication without Title', async () => {
+        await clearInputValue(await PublicationCreatePage.inputTitle);
+        await PublicationCreatePage.inputTitle.setValue('');
+        await PublicationCreatePage.btnSavePublication.click();
+        const requiredMessage = await getValidationMessage('title');
+        await expect(requiredMessage).toEqual(expected.general.errors.REQUIRED_FIELD);
+
+    });
+
+    it.only('Should not create the Publication without Content', async () => {
+        await PublicationCreatePage.fillAndSave('Google', 'Information about google company');
+        await expect(PublicationCreatePage.errorMessage).toBeDisplayed();
+        await expect(PublicationCreatePage.errorMessage).toHaveText(expected.publications.errors.CONTENT_REQUIRED_FIELD);
+
+    });
+});
